935d37405a9a32023d60cb982a1d7f03004a4eb6
nicolas ROCHE <nico_roche@hotmail.fr>
2020-07-07 19:20:03 +0200

    add pygame

---
+++ line_world_UI.py
@@ -0,0 +1,48 @@
+import pygame
+
+def display_line(win, num_states):
+    background_img = pygame.image.load("images/grid_cell.png").convert_alpha()
+    win.fill((0, 0, 0))
+    x_pos = 0
+    for y in range(0, num_states):
+        win.blit(background_img, (x_pos, 0))
+        x_pos += 100
+
+
+
+def event_loop():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                exit()
+
+
+
+def display_mouse(win, st, num_states):
+    cheese_img = pygame.image.load("images/mouse.png").convert_alpha()
+    cheese_img = pygame.transform.scale(cheese_img, (40, 40))
+
+    win.blit(cheese_img, (st * 100 + 30, 35))
+    pygame.display.flip()
+
+def display_reward(win, rewards, num_states):
+    cheese_img = pygame.image.load("images/cheese.png").convert_alpha()
+    cheese_img = pygame.transform.scale(cheese_img, (30, 30))
+
+    cheese2_img = pygame.image.load("images/cheese2.png").convert_alpha()
+    cheese2_img = pygame.transform.scale(cheese2_img, (30, 30))
+
+    cheese3_img = pygame.image.load("images/cheese3.png").convert_alpha()
+    cheese3_img = pygame.transform.scale(cheese3_img, (30, 30))
+
+    poison_img = pygame.image.load("images/poison.png").convert_alpha()
+    poison_img = pygame.transform.scale(poison_img, (30, 30))
+    for r in rewards:
+        if r[1] == 1:
+            win.blit(cheese_img, (r[0] * 100 + 35, 35))
+        if r[1] == -1:
+            win.blit(poison_img, (r[0] * 100 + 35, 35))
+
+
--- main.py
+++ main.py
@@ -6,11 +6,53 @@ from algo import iterative_policy_evaluation
 from grid_world import create_grid_world, tabular_uniform_random_policy, step, reset, is_terminal
 from grid_world_UI import display_grid, event_loop, display_reward, display_mouse
 
+from line_world import create_line_world, tabular_uniform_random_policy, step, reset, is_terminal
+from line_world_UI import display_line, event_loop, display_reward, display_mouse
 
-def test_grid_algo():
+
+
+def test_line_iterative_policy_evaluation():
     pygame.init()
 
-    w = 5
+    num_states = 7
+    rewards = ((0, -1),
+               (6, 1))
+
+    terminal = [0, 6]
+
+    S, A, T, P = create_line_world(num_states, rewards, terminal)
+    Pi = tabular_uniform_random_policy(S.shape[0], A.shape[0])
+
+    V = iterative_policy_evaluation(S, A, P, T, Pi)
+    print(V)
+
+
+    win = pygame.display.set_mode((num_states * 100, 100))
+
+    st = reset(num_states)
+
+    while not is_terminal(st, T):
+        display_line(win, num_states)
+        event_loop()
+        display_reward(win, rewards, num_states)
+        display_mouse(win, st, num_states)
+        sleep(1)
+
+        if V[st + 1] > V[st - 1] or V[st + 1] == 0:
+            a = 1
+        elif V[st + 1] <  V[st - 1] or V[st - 1] == 0:
+            a = 0
+        st, r, term = step(st, a, T, S, P)
+
+    display_line(win, num_states)
+    display_reward(win, rewards,num_states)
+    display_mouse(win, st, num_states)
+    sleep(1)
+
+def test_grid_iterative_policy_evaluation():
+    pygame.init()
+
+    w = 1
     h = 5
     rewards = ((24, 1),
                (2, -1),
@@ -53,5 +95,6 @@ def test_grid_algo():
     sleep(1)
 
 if __name__ == '__main__':
-    test_grid_algo()
+    #test_grid_iterative_policy_evaluation()
+    test_line_iterative_policy_evaluation()
 
